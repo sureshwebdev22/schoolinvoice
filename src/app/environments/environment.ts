fc87b59247bfdaa1e360662a41b18cbf4c9eb4f3
suresh <sureshwebdev22@gmail.com>
2026-07-29 11:00:48 +0530

    ec2 public ip changes

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://ec2-13-232-5-86.ap-south-1.compute.amazonaws.com:8080/api'
+  apiUrl: 'http://13.204.53.8:8080/api'
   
  // apiUrl: 'http://localhost:8080/api'
   
